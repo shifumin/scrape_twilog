f34dc9b630060d667172d85e07b3f94a8e63ab09
shifumin <shifumin4230@gmail.com>
2014-11-23 14:33:28 +0900

    一気に表示するように修正

--- scrape_twilog.rb
+++ scrape_twilog.rb
@@ -8,23 +8,32 @@ require 'nokogiri'
 # UserAgentをIEに偽装
 UserAgent = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)'
 
-# スクレイピング先のURL
-month = "month-1003/1"
-url = "http://twilog.org/shifumin/#{month}"
+# 何年何月
+month = 1004
+m = month.to_s
+# ページ数
+# max_page = 14
+max_page = [*(1..14)]
 
-charset = nil
-html = open(url, "User-Agent" => UserAgent) do |f|
-  charset = f.charset # 文字種別を取得
-  f.read # htmlを読み込んで変数htmlに渡す
+max_page.each do |page|
+
+  p = page.to_s
+  # スクレイピング先のURL
+  url = "http://twilog.org/shifumin/month-#{m}/#{p}"
+
+  charset = nil
+  html = open(url, "User-Agent" => UserAgent) do |f|
+    charset = f.charset # 文字種別を取得
+    f.read # htmlを読み込んで変数htmlに渡す
+  end
+
+  # htmlをパース(解析)してオブジェクトを生成
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+
+  doc.xpath('//p[@class = "tl-text"]').each do |node|
+   p node.text
+  end
+
+  p "\n"
+  sleep(2)
 end
-
-# htmlをパース(解析)してオブジェクトを生成
-doc = Nokogiri::HTML.parse(html, nil, charset)
-
-# タイトルを表示
-#p doc.title
-
-doc.xpath('//p[@class = "tl-text"]').each do |node|
-  p node.text
-end
-
