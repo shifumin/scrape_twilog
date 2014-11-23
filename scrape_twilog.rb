94bc7ba9f30f5bf0d71c70fa06ccba02f3b5dd91
shifumin <shifumin4230@gmail.com>
2014-11-23 22:26:55 +0900

    表示の""を除去 第1引数年月,第2引数ページ数

--- scrape_twilog.rb
+++ scrape_twilog.rb
@@ -8,18 +8,22 @@ require 'nokogiri'
 # UserAgentをIEに偽装
 UserAgent = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)'
 
-# 何年何月
-month = 1004
-m = month.to_s
+# 何年何月 2014年4月 1404
+month = ARGV[0]
+
 # ページ数
-# max_page = 14
-max_page = [*(1..14)]
+max_page = ARGV[1].to_i
+page_a = Array.new
+# page_a = [1..(max_page)]したい
+max_page.times do |p|
+  page_a << p + 1
+end
 
-max_page.each do |page|
 
+page_a.each do |page|
   p = page.to_s
   # スクレイピング先のURL
-  url = "http://twilog.org/shifumin/month-#{m}/#{p}"
+  url = "http://twilog.org/shifumin/month-#{month}/#{p}"
 
   charset = nil
   html = open(url, "User-Agent" => UserAgent) do |f|
@@ -30,10 +34,17 @@ max_page.each do |page|
   # htmlをパース(解析)してオブジェクトを生成
   doc = Nokogiri::HTML.parse(html, nil, charset)
 
+  doc2 = doc.xpath('//h3[@class = "title01"]')
+  puts doc2.xpath('a').text
+  puts doc2.xpath('span').text
+  
+  # 本文を抽出してリプライ以外のツイートを表示
   doc.xpath('//p[@class = "tl-text"]').each do |node|
-   p node.text
+    if node.xpath('a').text[0] != "@"
+      puts node.text
+    end
   end
 
-  p "\n"
-  sleep(2)
+  # 1秒間だけ待ってやる
+  sleep(1)
 end
