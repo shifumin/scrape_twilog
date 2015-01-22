54821b2c41026b25646195a5603df1cbe32e31c3
shifumin <shifumin4230@gmail.com>
2015-01-22 20:08:37 +0900

    標準入力から何年何月のTwilogからスクレイピングするかを確認するように修正

--- twilog_scraping.rb
+++ twilog_scraping.rb
@@ -11,8 +11,23 @@ require 'nokogiri'
 # UserAgentをIEに偽装
 UserAgent = 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)'
 
+
+# 標準入力から何年何月のものをスクレイピングするか取得
+puts "何年？  例:2014"
+year = STDIN.gets
+year.chomp!
+
+puts "何月？  例:04"
+month =STDIN.gets
+month.chomp!
+
+y_m = year[-2, 2] + month
+
+
+=begin
 # 何年何月か 2014年4月なら201404と入力されるので、monthに1404を格納する
 month = ARGV[0][-4, 4]
+=end
 
 # 各ページごとの処理
 catch(:break_loop) do
@@ -22,7 +37,7 @@ catch(:break_loop) do
     page += 1    
     p = page.to_s
     # スクレイピング先のURL
-    url = "http://twilog.org/shifumin/month-#{month}/#{p}"
+    url = "http://twilog.org/shifumin/month-#{y_m}/#{p}"
 
     charset = nil
     html = open(url, "User-Agent" => UserAgent) do |f|
